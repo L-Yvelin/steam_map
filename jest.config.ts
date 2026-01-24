308514e3a293d5a5521e0051e057cad9889bade1
github-louis-yvelin <louis.yvelin@teads.tv>
2026-01-24 14:22:52 +0100

    test: Add country matcher tests and configuration

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+/** @jest-config-loader ts-node */
+import type { Config } from "jest";
+
+const config: Config = {
+  verbose: true,
+  preset: "ts-jest",
+  testEnvironment: "node",
+};
+export default config;
+
